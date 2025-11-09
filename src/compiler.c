f9e47b59d6c33132539aaa53204a58760017b735
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2025-11-09 09:57:22 -0500

    Set up helper funcs for compiler

--- src/compiler.c
+++ src/compiler.c
@@ -11,6 +11,9 @@ void consume(TokenType, const char*);
 void error(Token, const char*);
 void errorAt(Token*, const char*);
 void expression();
+void emitByte(uint8_t byte);
+void endCompiler();
+void emitReturn();
 
 typedef struct {
   Token previous;
@@ -19,15 +22,23 @@ typedef struct {
   bool  panicMode;
 } Parser;
 
+static Chunk* compilingChunk;
+
 Parser parser;
+
+Chunk* currentChunk(){
+  return compilingChunk;
+}
 
 bool compile(const char* source, Chunk* chunk){
   initScanner(source);
+  compilingChunk = chunk;
   parser.hadError  = false;
   parser.panicMode = false;
   advance();
   expression();
   consume(TOKEN_EOF, "Expected EOF Expression");
+  endCompiler();
   return !parser.hadError;
 }
 
@@ -71,5 +82,22 @@ void errorAt(Token* token, const char* message){
   }
   fprintf(stderr, ": %s\n", message);
   parser.hadError = true;
+}
+
+void emitByte(uint8_t byte){
+  writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+void emitBytes(uint8_t byte1, uint8_t byte2){
+  emitByte(byte1);
+  emitByte(byte2);
+}
+
+void endCompiler(){
+  emitReturn();
+}
+
+void emitReturn(){
+  writeChunk(currentChunk(), OP_RETURN, parser.previous.line);
 }
 
