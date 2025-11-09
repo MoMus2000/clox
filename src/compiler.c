8798bcf7a884866c73c2f17b06c0c32e504e1902
Mustafa Muhammad <muhammadmustafa4000@gmail.com>
2025-11-09 09:09:10 -0500

    Parser frontend

--- src/compiler.c
+++ src/compiler.c
@@ -1,20 +1,75 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "chunk.h"
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char* source){
+void static advance();
+void static errorAtCurrent(const char*);
+void consume(TokenType, const char*);
+void error(Token, const char*);
+void errorAt(Token*, const char*);
+void expression();
+
+typedef struct {
+  Token previous;
+  Token current;
+  bool  hadError;
+  bool  panicMode;
+} Parser;
+
+Parser parser;
+
+bool compile(const char* source, Chunk* chunk){
   initScanner(source);
-  int line = -1;
+  parser.hadError  = false;
+  parser.panicMode = false;
+  advance();
+  expression();
+  consume(TOKEN_EOF, "Expected EOF Expression");
+  return !parser.hadError;
+}
+
+void static advance(){
+  parser.previous = parser.current;
   for(;;){
     Token token = scanToken();
-    if(token.line != line){
-      printf("%4d ", token.line);
-      line = token.line;
-    }
-    else {
-      printf("   | ");
-    }
-    printf("%2d '%.*s'\n", token.type, token.length, token.start);
-    if (token.type == TOKEN_EOF) break;
+    parser.current = token;
+    if(parser.current.type != TOKEN_ERROR) break;
+    errorAtCurrent(parser.current.start);
   }
 }
+
+void static errorAtCurrent(const char* message){
+  errorAt(&parser.current, message);
+}
+
+void consume(TokenType type, const char* message){
+  if(parser.current.type == type){
+    advance();
+    return;
+  }
+  errorAtCurrent(message);
+}
+
+void expression(){
+}
+
+void errorAt(Token* token, const char* message){
+  if(parser.panicMode) return;
+  parser.panicMode = true;
+  fprintf(stderr, "[line %d] Error", token->line);
+  if(token->type == TOKEN_EOF){
+    fprintf(stderr, " at end");
+  }
+  else if(token->type == TOKEN_ERROR){
+    //
+  }
+  else {
+    fprintf(stderr, " at '%.*s'", token->length, token->start);
+  }
+  fprintf(stderr, ": %s\n", message);
+  parser.hadError = true;
+}
+
